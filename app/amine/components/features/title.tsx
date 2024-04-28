ec4f9221b645b607b060aa30a6370e81a87d181d
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-04-28 16:33:43 +0330

    amie p1

---
+++ app/amine/components/features/title.tsx
@@ -0,0 +1,52 @@
+'use client'
+import classNames from 'classnames'
+import { useInView, motion } from 'framer-motion'
+import React, { useEffect, useRef } from 'react'
+import { useFeatureStore } from './store'
+
+type Props = {
+  children: React.ReactNode
+  id: string
+}
+
+export const FeatureTitle = ({ children, id }: Props) => {
+  const ref = useRef<HTMLParagraphElement>(null)
+  // const documentRef = useRef(document)
+  const isInView = useInView(ref, {
+    margin: '-50% 0px -50% 0px',
+    // NOTE: The only reason we pass in the document here, is because
+    // of security restrictions set by the browser when using an iFrame.
+    // In an iFrame (so eg in the preview on frontend.fyi),
+    // margin won't take effect unless you specify the root manually.
+    // By default it will be the window element, which is what we want in this case.
+    // If you specify your own root, you can usually only pass in an Element, and
+    // not the document (since document/window is the default). However, in order
+    // to fix the issue in the iframe, we need to pass in the document here and thus
+    // tell TypeScript that we know what we're doing. If you're implementing
+    // this in your own website, you can just pass in the root property as well as the documentRef.
+    // eslint-disable-next.line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // root: documentRef,
+  })
+  const setInViewFeature = useFeatureStore((state) => state.setInViewFeature)
+  const inViewFeature = useFeatureStore((state) => state.inViewFeature)
+
+  useEffect(() => {
+    if (isInView) setInViewFeature(id)
+    if (!isInView && inViewFeature === id) setInViewFeature(null)
+  }, [isInView, id, setInViewFeature, inViewFeature])
+
+  return (
+    <motion.p
+      ref={ref}
+      className={classNames(
+        'feature-title py-16 font-heading text-5xl transition-colors text-gray-800'
+        // isInView ? 'text-black' : 'text-gray-300'
+      )}
+      whileInView={{ color: 'white' }}
+      viewport={{ margin: '-50% 0px -50% 0px' }}
+    >
+      {children}
+    </motion.p>
+  )
+}
---
+++ app/amine/page.tsx
@@ -0,0 +1,11 @@
+import App from './components/App'
+
+function page() {
+  return (
+    <div>
+      <App />
+    </div>
+  )
+}
+
+export default page
